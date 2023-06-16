bd1ccec138b9a4b269e487f6aa7709dce1a3c4ad
illia.budarov <illia.budarov@volya.us>
2023-06-16 13:17:24 +0300

    updated CMakeLists.txt & added MainWindow

---
+++ 3DViewer/UI/include/3DViewer.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <QMainWindow>
+
+class Viewer : public QMainWindow
+{
+};
--- 3DViewer/main.cpp
+++ 3DViewer/main.cpp
@@ -1,8 +1,13 @@
 #include <QApplication>
+#include "UI/include/3DViewer.h"
 
 int main(int argc, char *argv[]) {
 
 	QApplication app(argc, argv);
-    QGuiApplication::setApplicationDisplayName("3DViewer");
+    QGuiApplication::setApplicationDisplayName(Viewer::tr("3DViewer"));
+
+    Viewer viewer;
+    viewer.show();
+
     return app.exec();
-}
+}   
